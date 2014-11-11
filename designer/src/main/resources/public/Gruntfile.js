2e53e71627ca66acfc97dc2579a513c441df887a
lizhantong <lztlovely@126.com>
2014-11-11 15:42:19 +0800

    remove unused code

--- designer/src/main/resources/public/Gruntfile.js
+++ designer/src/main/resources/public/Gruntfile.js
@@ -9,7 +9,7 @@
 module.exports = function(grunt) {
     start();
     // test();
-
+    // alert(grunt);
     // TODO ----------------------------------------------------------
     // (-) change skin
 
