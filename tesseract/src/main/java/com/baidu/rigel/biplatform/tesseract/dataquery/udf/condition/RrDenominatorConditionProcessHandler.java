cda6af5627cf6a84db7cb343d61f36b7d1200a30
wangyuxue <wangyuxue2007@sina.com>
2014-12-29 09:14:46 +0800

    support for udf

--- tesseract/src/main/java/com/baidu/rigel/biplatform/tesseract/dataquery/udf/condition/RrDenominatorConditionProcessHandler.java
+++ tesseract/src/main/java/com/baidu/rigel/biplatform/tesseract/dataquery/udf/condition/RrDenominatorConditionProcessHandler.java
@@ -15,6 +15,13 @@
  */
 package com.baidu.rigel.biplatform.tesseract.dataquery.udf.condition;
 
+import java.util.Calendar;
+import java.util.Date;
+
+import com.baidu.rigel.biplatform.ac.minicube.TimeDimension;
+import com.baidu.rigel.biplatform.ac.model.TimeType;
+import com.baidu.rigel.biplatform.ac.util.TimeRangeDetail;
+import com.baidu.rigel.biplatform.ac.util.TimeUtils;
 import com.baidu.rigel.biplatform.tesseract.qsservice.query.vo.QueryContext;
 
 /**
@@ -24,8 +31,54 @@ import com.baidu.rigel.biplatform.tesseract.qsservice.query.vo.QueryContext;
  */
 class RrDenominatorConditionProcessHandler  extends RateConditionProcessHandler {
 
+	/**
+	 * {@inheritDoc}
+	 */
 	@Override
 	public QueryContext processCondition(QueryContext context) {
+		/*
+		 * 环比分母计算条件，修改时间条件为上一个统计周期
+		 * 如果时间粒度是天，则上一个统计周期为昨天
+		 * 如果时间粒度是周，则上一个统计周期为上周
+		 * 如果时间粒度是月，则上一个统计周期为上月
+		 * 如果时间粒度是季度，则上一个统计周期为上季度
+		 * 如果时间粒度是年，则上一个统计周期为去年
+		 * 需要QueryContext提供：时间维度定义，当前时间维度过滤条件，此处修改时间范围后，重新设置到查询上下文
+		 */
+		// TODO 确认一下如何获取时间维度并获取当前时间粒度第一天
+		TimeDimension dimension = null;
+		TimeType timeType = dimension.getDataTimeType();
+		Date firstDayOfTimeRange = null;
+		Calendar cal = Calendar.getInstance();
+		cal.setTime(firstDayOfTimeRange);
+		// 将时间调整为上一统计周期最后一天
+		cal.add(Calendar.DAY_OF_YEAR, -1);
+		TimeRangeDetail timeRange = null;
+		switch (timeType) {
+			case TimeDay:
+				timeRange = TimeUtils.getDays(cal.getTime(), 0, 0);
+				break;
+			case TimeWeekly:
+				timeRange = TimeUtils.getWeekDays(cal.getTime());
+				break;
+			case TimeMonth:
+				timeRange = TimeUtils.getMonthDays(cal.getTime());
+				break;
+			case TimeQuarter:
+				timeRange = TimeUtils.getQuarterDays(cal.getTime());
+				break;
+			case TimeYear:
+				timeRange = TimeUtils.getYearDays(cal.getTime());
+				break;
+			default:
+				throw new RuntimeException("未知的时间维度类型：" + timeType.name());
+		}
+		if (timeRange == null) {
+			throw new RuntimeException("未知的时间维度类型：" + timeType.name());
+		}
+		// TODO 确认当前统计周期的成员数量，从前一统计周期获取相同数量成员
+		String[] days = timeRange.getDays();
+		// TODO 确认一下如何修改条件并返回context
 		return null;
 	}
 
--- tesseract/src/main/java/com/baidu/rigel/biplatform/tesseract/dataquery/udf/condition/SrDenominatorConditionProcessHandler.java
+++ tesseract/src/main/java/com/baidu/rigel/biplatform/tesseract/dataquery/udf/condition/SrDenominatorConditionProcessHandler.java
@@ -15,22 +15,86 @@
  */
 package com.baidu.rigel.biplatform.tesseract.dataquery.udf.condition;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
+import javax.management.RuntimeErrorException;
+
+import com.baidu.rigel.biplatform.ac.minicube.TimeDimension;
+import com.baidu.rigel.biplatform.ac.model.TimeType;
+import com.baidu.rigel.biplatform.ac.util.TimeRangeDetail;
+import com.baidu.rigel.biplatform.ac.util.TimeUtils;
 import com.baidu.rigel.biplatform.tesseract.qsservice.query.vo.QueryContext;
 
 /**
  * Description: SrDenominatorConditionProcessHandler
+ * 
  * @author david.wang
  *
  */
 class SrDenominatorConditionProcessHandler extends RateConditionProcessHandler {
 
-	/* 
+	/*
 	 * (non-Javadoc)
-	 * @see com.baidu.rigel.biplatform.tesseract.dataquery.udf.condition.AbsRateConditionProcessHandler#processCondition(com.baidu.rigel.biplatform.tesseract.qsservice.query.vo.QueryContext)
+	 * 
+	 * @see com.baidu.rigel.biplatform.tesseract.dataquery.udf.condition.
+	 * AbsRateConditionProcessHandler
+	 * #processCondition(com.baidu.rigel.biplatform
+	 * .tesseract.qsservice.query.vo.QueryContext)
 	 */
 	@Override
 	public QueryContext processCondition(QueryContext context) {
-		// TODO Auto-generated method stub
+		/*
+		 * 同比分母计算条件，修改时间条件为同期时间 如果时间粒度是天，则同期时间为去年的当天 如果时间粒度是周，则同期时间为去年的当周
+		 * 如果时间粒度是月，则同期时间为去年的当月 如果时间粒度是季度，则统计周期为去年的当季度 如果时间粒度是年，则同期时间为去年
+		 * 需要QueryContext提供：时间维度定义，当前时间维度过滤条件，此处修改时间范围后，重新设置到查询上下文
+		 */
+		// TODO 确认一下如何获取时间维度并获取当前时间粒度第一天
+		TimeDimension dimension = null;
+		TimeType timeType = dimension.getDataTimeType();
+		Date firstDayOfTimeRange = null;
+		Calendar cal = Calendar.getInstance();
+		cal.setTime(firstDayOfTimeRange);
+		// 将时间调整为上年的同天
+		cal.add(Calendar.YEAR, -1);
+		TimeRangeDetail timeRange = null;
+		switch (timeType) {
+		case TimeDay:
+			timeRange = TimeUtils.getDays(cal.getTime(), 0, 0);
+			break;
+		case TimeWeekly:
+			// 星期需要特殊处理
+			try {
+				SimpleDateFormat sdf = new SimpleDateFormat();
+				sdf.applyPattern(TimeRangeDetail.FORMAT_STRING);
+				int weekIndex = TimeUtils.getWeekIndex(sdf.format(firstDayOfTimeRange));
+				cal.set(Calendar.MONTH, 0);
+				cal.set(Calendar.DAY_OF_YEAR, 1);
+				cal.add(Calendar.DAY_OF_YEAR, 7 * weekIndex);
+				timeRange = TimeUtils.getWeekDays(cal.getTime());
+			} catch (Exception e) {
+				throw new RuntimeException(e);
+			}
+			break;
+		case TimeMonth:
+			timeRange = TimeUtils.getMonthDays(cal.getTime());
+			break;
+		case TimeQuarter:
+			timeRange = TimeUtils.getQuarterDays(cal.getTime());
+			break;
+		case TimeYear:
+			timeRange = TimeUtils.getYearDays(cal.getTime());
+			break;
+		default:
+			throw new RuntimeException("未知的时间维度类型：" + timeType.name());
+		}
+		if (timeRange == null) {
+			throw new RuntimeException("未知的时间维度类型：" + timeType.name());
+		}
+		// TODO 确认当前统计周期的成员数量，从前一统计周期获取相同数量成员
+		String[] days = timeRange.getDays();
+		// TODO 确认一下如何修改条件并返回context
 		return null;
 	}
 
