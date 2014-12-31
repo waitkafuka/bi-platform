98902669f0989913aad1459687ae6abe762c8b4b
weiboxue <wbx_901118@sina.com>
2014-12-31 16:32:14 +0800

    指标非空判断问题以及样式修改

--- designer/src/main/resources/public/silkroad/src/report/component-box/components/select-vm-template.js
+++ designer/src/main/resources/public/silkroad/src/report/component-box/components/select-vm-template.js
@@ -2,9 +2,9 @@ define(['template'], function (template) {
     function anonymous($data,$filename) {
         'use strict';
         $data=$data||{};
-        var $utils=template.utils,$helpers=$utils.$helpers,$escape=$utils.$escape,id=$data.id,$out='';$out+='<div class="comp-box di-o_o-line">\n    <div data-o_o-di="';
+        var $utils=template.utils,$helpers=$utils.$helpers,$escape=$utils.$escape,id=$data.id,$out='';$out+='<div class="comp-box di-o_o-line">\r\n    <div data-o_o-di="';
         $out+=$escape(id);
-        $out+='">\n    </div>\n</div>';
+        $out+='">\r\n    </div>\r\n</div>';
         return $out;
     }
     return { render: anonymous };
