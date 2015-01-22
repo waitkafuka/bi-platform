8df33b4d3a52b0778bdfecd7f0b16063151f4e26
chenxiaoming01 <chenxiaoming01@baidu.com>
2015-01-22 18:39:49 +0800

    整理silkroad端皮肤结构

---
+++ designer/src/main/resources/public/silkroad/asset/sk/report/global-menu-btns/component-menu-template.js
@@ -0,0 +1,1 @@
+define(["template"],function(a){function b(b){"use strict";b=b||{};var c=a.utils,d=(c.$helpers,"");return d+='<div class="global-menus component-menu j-all-menus" id="component">\n</div>'}return{render:b}});
