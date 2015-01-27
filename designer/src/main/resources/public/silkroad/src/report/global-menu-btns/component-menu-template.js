c37d4f34136eb793fe33fc08791702dc899abda6
weiboxue <wbx_901118@sina.com>
2015-01-27 17:18:41 +0800

    商桥皮肤，表格数据居中，问号，排序图片修改

--- designer/src/main/resources/public/silkroad/src/report/global-menu-btns/component-menu-template.js
+++ designer/src/main/resources/public/silkroad/src/report/global-menu-btns/component-menu-template.js
@@ -2,7 +2,7 @@ define(['template'], function (template) {
     function anonymous($data,$filename) {
         'use strict';
         $data=$data||{};
-        var $utils=template.utils,$helpers=$utils.$helpers,$out='';$out+='<div class="global-menus component-menu j-all-menus" id="component">\n</div>\n<div class="global-menus skin-menu j-all-menus" id="skin-report">\n    <div class="skin-menu-box">\n        <div class=\'skin-type\'><div class="classic-skin skin-pic"></div><div class="skin-name">经典</div></div>\n        <div class=\'skin-type\' style="margin-right: 5px;"><div class="lightBlue-skin skin-pic"></div><div class="skin-name">商桥</div></div>\n    </div>\n</div>';
+        var $utils=template.utils,$helpers=$utils.$helpers,$out='';$out+='<div class="global-menus component-menu j-all-menus" id="component">\n</div>\n<div class="global-menus skin-menu j-all-menus" id="skin-report">\n    <div class="skin-menu-box">\n        <div class=\'skin-type\'><div class="classic-skin skin-pic"></div><div class="skin-name">经典</div></div>\n        <div class=\'skin-type\'><div class="lightBlue-skin skin-pic"></div><div class="skin-name">商桥</div></div>\n    </div>\n</div>';
         return $out;
     }
     return { render: anonymous };
