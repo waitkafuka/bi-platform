301ca216545187e7951215ab1650c7f5cc8eda7d
weiboxue <wbx_901118@sina.com>
2015-02-11 20:36:04 +0800

    查询按钮更改为商桥样式

--- designer/src/main/resources/public/silkroad/src/report/edit/canvas/canvas-view.js
+++ designer/src/main/resources/public/silkroad/src/report/edit/canvas/canvas-view.js
@@ -339,7 +339,7 @@ define([
                 that.dragWidthHeight($component, 'MULTISELECT', 47, 47);
                 // 固定文本框的高度
                 that.dragWidthHeight($component, 'TEXT', 50, 50);
-                that.dragWidthHeight($component, 'H_BUTTON', 50, 50);
+                that.dragWidthHeight($component, 'H_BUTTON', 55, 55);
                 // 删除参考线-避免重复渲染产生多余的参考线
                 that.removeGuides($component);
                 // 调整后添加参考线
