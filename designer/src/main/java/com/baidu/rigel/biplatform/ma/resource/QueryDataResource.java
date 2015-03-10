f1b0ef1f02b920f2ec4147ca17546a472dc99e94
wangyuxue <wangyuxue@baidu.com>
2015-03-10 15:21:09 +0800

    面包屑问题修改

--- designer/src/main/java/com/baidu/rigel/biplatform/ma/resource/QueryDataResource.java
+++ designer/src/main/java/com/baidu/rigel/biplatform/ma/resource/QueryDataResource.java
@@ -687,6 +687,7 @@ public class QueryDataResource extends BaseResource {
          * 查询区域的时候，会按照当前的参数更新区域上下文
          */
         QueryContext localContext = runTimeModel.getLocalContextByAreaId(areaId);
+        localContext.reset ();
         for (String key : contextParams.keySet()) {
             /**
              * 更新runtimeModel的区域上下文参数
@@ -898,7 +899,7 @@ public class QueryDataResource extends BaseResource {
                     mainDims.add(root);
                     Collections.reverse(mainDims);
                     areaContext.setCurBreadCrumPath(root);
-//                    resultMap.put("mainDimNodes", mainDims);
+                    resultMap.put("mainDimNodes", mainDims);
                 } else {
                     areaContext.setCurBreadCrumPath (Maps.newHashMap ());
 //                    resultMap.put("mainDimNodes", areaContext.getCurBreadCrumPath ());
@@ -1256,8 +1257,9 @@ public class QueryDataResource extends BaseResource {
                 mainDims.add(root);
             }
             Collections.reverse(mainDims);
-//            resultMap.put("mainDimNodes", mainDims);
-            runTimeModel.getContext().put("bread_key", mainDims);
+            resultMap.put("mainDimNodes", mainDims);
+            areaContext.getParams ().put ("bread_key", mainDims);
+//            runTimeModel.getContext().put("bread_key", mainDims);
         } 
         areaContext.getQueryStatus().add(result);
         // 更新局部区域参数，避免漏掉当前请求查询的
@@ -1447,16 +1449,18 @@ public class QueryDataResource extends BaseResource {
             resultMap.put("pivottable", table);
             resultMap.put("rowCheckMin", 1);
             resultMap.put("rowCheckMax", 5);
-            Object breadCrum = runTimeModel.getContext().get("bread_key");
-            if (breadCrum == null) {
-                List<Map<String, String>> tmp = Lists.newArrayList();
-                if (areaContext.getCurBreadCrumPath() != null  && !areaContext.getCurBreadCrumPath().isEmpty()) {
-                    tmp.add(areaContext.getCurBreadCrumPath());
-                    breadCrum = tmp;
+            if (targetArea.getLogicModel ().getRows ().length >= 2) {
+                Object breadCrum = areaContext.getParams ().get("bread_key");
+                if (breadCrum == null) {
+                    List<Map<String, String>> tmp = Lists.newArrayList();
+                    if (areaContext.getCurBreadCrumPath() != null  && !areaContext.getCurBreadCrumPath().isEmpty()) {
+                        tmp.add(areaContext.getCurBreadCrumPath());
+                        breadCrum = tmp;
+                    }
                 }
-            }
-            if (breadCrum != null) {
-//                resultMap.put("mainDimNodes", breadCrum);
+                if (breadCrum != null) {
+                    resultMap.put("mainDimNodes", breadCrum);
+                }
             }
             resultMap.put("reportTemplateId", reportId);
             resultMap.put("totalSize", table.getActualSize());
