9103a37904fe01d3a693ee74fff2b992a97e0c27
weiboxue <wbx_901118@sina.com>
2015-04-15 16:03:57 +0800

    日历控件维度更新操作问题修改

--- designer/src/main/resources/public/silkroad/src/report/edit/canvas/comp-setting-time-template.js
+++ designer/src/main/resources/public/silkroad/src/report/edit/canvas/comp-setting-time-template.js
@@ -2,10 +2,10 @@ define(['template'], function (template) {
     function anonymous($data,$filename) {
         'use strict';
         $data=$data||{};
-        var $utils=template.utils,$helpers=$utils.$helpers,$escape=$utils.$escape,compId=$data.compId,$each=$utils.$each,candDims=$data.candDims,$dim=$data.$dim,$index=$data.$index,$out='';$out+='<div class="con-comp-setting-type1 j-comp-setting" data-comp-id="';
+        var $utils=template.utils,$helpers=$utils.$helpers,$escape=$utils.$escape,compId=$data.compId,$each=$utils.$each,xAxis=$data.xAxis,$dim=$data.$dim,$index=$data.$index,$out='';$out+='<div class="con-comp-setting-type1 j-comp-setting" data-comp-id="';
         $out+=$escape(compId);
         $out+='" data-comp-type="TIME_COMP">\r\n    <div class="data-axis-line data-axis-line-48 j-comp-setting-line j-line-x" data-axis-type="x">\r\n        <span class="letter">时间维度:</span>\r\n        ';
-        $each(candDims,function($dim,$index){
+        $each(xAxis,function($dim,$index){
         $out+='\r\n        <div class="item hover-bg c-m j-root-line  ui-draggable j-can-to-ind ui-draggable-dragging olap-element-dragging" data-id="';
         $out+=$escape($dim.id);
         $out+='" data-name="';
