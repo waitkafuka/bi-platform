c0b54beb6abefec75b0ec604130e0302967b6833
wangyuxue <sishuiliunian2004@163.com>
2015-05-13 16:33:56 +0800

    将数据源元数据修改为注册方式

--- designer/src/main/java/com/baidu/rigel/biplatform/ma/ds/service/DataSourceInfoReaderServiceFactory.java
+++ designer/src/main/java/com/baidu/rigel/biplatform/ma/ds/service/DataSourceInfoReaderServiceFactory.java
@@ -1,11 +1,6 @@
 package com.baidu.rigel.biplatform.ma.ds.service;
 
-import java.util.Map;
-
 import com.baidu.rigel.biplatform.ma.ds.exception.DataSourceOperationException;
-import com.baidu.rigel.biplatform.ma.ds.service.impl.RelationDBInfoReaderServiceImpl;
-import com.baidu.rigel.biplatform.ma.model.ds.DataSourceType;
-import com.google.common.collect.Maps;
 /**
  * 数据源信息读取服务DataSourceInfoReader的实例化工厂类
  * @author jiangyichao
@@ -15,30 +10,7 @@ public final class DataSourceInfoReaderServiceFactory {
     
     private DataSourceInfoReaderServiceFactory () {
     }
-    
-    /**
-     * 元数据服务实例仓库
-     */
-    private static final Map<String, DataSourceInfoReaderService> SERVICE_REPOSITORY = Maps.newHashMap ();
-    
-     static {
-         SERVICE_REPOSITORY.put (DataSourceType.MYSQL.name (), new RelationDBInfoReaderServiceImpl ());
-         SERVICE_REPOSITORY.put (DataSourceType.H2.name (), new RelationDBInfoReaderServiceImpl ());
-         SERVICE_REPOSITORY.put (DataSourceType.ORACLE.name (), new RelationDBInfoReaderServiceImpl ());
-         SERVICE_REPOSITORY.put (DataSourceType.MYSQL_DBPROXY.name (), new RelationDBInfoReaderServiceImpl ());
-     }
      
-     /**
-      * 依据datasourceType注册相应元数据相关服务
-      * @param type 数据源类型
-      * @param service 数据源元数据服务
-      */
-     public static void registryDataSourceService (String type, DataSourceInfoReaderService service) {
-         if (SERVICE_REPOSITORY.containsKey (type)) {
-             throw new IllegalStateException ("can not support registry mutilple service for type :" + type);
-         }
-         SERVICE_REPOSITORY.put (type, service);
-     }
     /**
      * 
      * @param type
@@ -47,7 +19,7 @@ public final class DataSourceInfoReaderServiceFactory {
      */
     public static DataSourceInfoReaderService getDataSourceInfoReaderServiceInstance(
             String type) throws DataSourceOperationException {
-        return SERVICE_REPOSITORY.get (type);
+        return DataSourceMetaServiceHelper.getDsMetaService (type);
 //        switch (dataDourceType) {
 //        // 关系数据库
 //            case MYSQL:
--- designer/src/main/java/com/baidu/rigel/biplatform/ma/ds/utils/DataSourceUtil.java
+++ designer/src/main/java/com/baidu/rigel/biplatform/ma/ds/utils/DataSourceUtil.java
@@ -11,94 +11,113 @@ import com.baidu.rigel.biplatform.ma.report.utils.ContextManager;
 
 /**
  * 数据源工具类
+ * 
  * @author jiangyichao
  *
  */
 @Service
 public final class DataSourceUtil {
-	
-	/**
-	 * 数据源组路径
-	 */
-    private static String dsFileBaseDir; 
+    
+    /**
+     * 数据源组路径
+     */
+    private static String dsFileBaseDir;
     
     /**
      * 注入数据源组存放路径
+     * 
      * @param dsDir
      */
     @Value("${biplatform.ma.ds.location}")
     public void setDsFileBaseDir(String dsDir) {
-    	DataSourceUtil.dsFileBaseDir = dsDir;
+        DataSourceUtil.dsFileBaseDir = dsDir;
     }
+    
     /**
      * 获取数据源文件的文件名（含路径）
-     * @param ds 数据源定义
+     * 
+     * @param ds
+     *            数据源定义
      * @return 返回数据源定义文件文件名（绝对路径）
      */
     public static String getDsFileName(DataSourceDefine ds) {
-        String basePath = getDsFileStoreDir();
-        return basePath + File.separator + ds.getId() + "_" + ds.getName();
+        String basePath = getDsFileStoreDir ();
+        return basePath + File.separator + ds.getId () + "_" + ds.getName ();
     }
     
     /**
      * 根据文件名，返回文件的详细路径
-     * @param fileName 文件名
+     * 
+     * @param fileName
+     *            文件名
      * @return 文件路径信息
      */
     public static String getDsFileName(String fileName) {
-        String basePath = getDsFileStoreDir();
+        String basePath = getDsFileStoreDir ();
         return basePath + File.separator + fileName;
     }
+    
     /**
      * 获取数据源组文件的文件名（含路径）
-     * @param dsG 数据源组定义
+     * 
+     * @param dsG
+     *            数据源组定义
      * @return 返回数据源组定义文件文件名(绝对路径)
      */
     public static String getDsGroupFileName(DataSourceGroupDefine dsG) {
-    	String basePath = getDsGroupFileStoreDir();
-    	return basePath + File.separator + dsG.getId() + "_" + dsG.getName();
+        String basePath = getDsGroupFileStoreDir ();
+        return basePath + File.separator + dsG.getId () + "_" + dsG.getName ();
     }
     
     /**
      * 根据文件名，返回文件的详细路径信息
-     * @param fileName 文件名称
+     * 
+     * @param fileName
+     *            文件名称
      * @return 文件的详细路径
      */
     public static String getDsGroupFileName(String fileName) {
-    	return getDsFileName(fileName);
+        return getDsFileName (fileName);
     }
+    
     /**
      * 获取当前产品线数据源的存储路径
+     * 
      * @return 数据源存储路径
      */
     public static String getDsFileStoreDir() {
-    	return getDsFileStoreDir(ContextManager.getProductLine());
+        return getDsFileStoreDir (ContextManager.getProductLine ());
     }
     
     /**
      * 获取某产品线下的数据源存储路径
-     * @param productLine 产品线
+     * 
+     * @param productLine
+     *            产品线
      * @return 数据源存储路径
      */
     public static String getDsFileStoreDir(String productLine) {
-    	String basePath = productLine + File.separator + dsFileBaseDir;
-    	return basePath;
+        String basePath = productLine + File.separator + dsFileBaseDir;
+        return basePath;
     }
     
     /**
      * 获取数据源组的存储路径
+     * 
      * @return 数据源组存储路径
      */
     public static String getDsGroupFileStoreDir() {
-    	return getDsFileStoreDir();
+        return getDsFileStoreDir ();
     }
     
     /**
      * 获取某产品线下数据源组存储路径
-     * @param productLine 产品线
+     * 
+     * @param productLine
+     *            产品线
      * @return 数据源组存储路径
      */
     public static String getDsGroupFileStoreDir(String productLine) {
-    	return getDsFileStoreDir(productLine);
+        return getDsFileStoreDir (productLine);
     }
 }
