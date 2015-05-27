0027accc2f8005a477f76a38937da64c8846bd6c
wangyuxue <sishuiliunian2004@163.com>
2015-05-27 13:18:49 +0800

    add comment

--- tesseract/src/main/java/com/baidu/rigel/biplatform/tesseract/util/QueryRequestUtil.java
+++ tesseract/src/main/java/com/baidu/rigel/biplatform/tesseract/util/QueryRequestUtil.java
@@ -350,7 +350,7 @@ public class QueryRequestUtil {
     private static PullUpProperties coolectAllMem(MemberNodeTree memberNodeTree) {
         PullUpProperties result = null;
         if (StringUtils.isNotBlank(memberNodeTree.getName()) && memberNodeTree.getChildren().size() >= 1) {
-            // TODO 解决callback维度下钻返回多层，汇总数据计算错误问题
+            // Fixed by Me David.wang 解决callback维度下钻返回多层，汇总数据计算错误问题
             if(memberNodeTree.getChildren().size() == 1 
                     && memberNodeTree.getChildren().get(0).getChildren ().size () > 1) {
                 return coolectAllMem(memberNodeTree.getChildren().get(0));
